9beb7c1c7143d1d7ae00a077c9a6052d7ed65a08
julio <julio@OsmerDavidCardona>
2024-11-07 21:08:45 -0600

    Tarea Grupal

--- src/gestionpersonas/Main.java
+++ src/gestionpersonas/Main.java
@@ -11,7 +11,7 @@ public class Main
         // Llamadas polimórficas al método mostrarInfo
         doctor.mostrarInfo();
         deportista.mostrarInfo();
-        //Finalizacion  2222
+        //Tarea Grupal
     }
 }
 
